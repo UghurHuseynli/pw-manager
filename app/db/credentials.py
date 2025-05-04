474a72bda5e8c6c3fee05bc81cbe4782cbb04397
Ughur Huseynli <u.hub.programming@gmail.com>
2025-05-04 19:48:44 +0400

    alembic setup

--- app/db/credentials.py
+++ app/db/credentials.py
@@ -1,15 +1,35 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime, timezone
+import uuid
 
 
-class Credentials(SQLModel, table=True):
-    id: int = Field(primary_key=True)
-    user_id: int = Field(foreign_key="user.id")
-    service_name: str
+class CredentialsBase(SQLModel):
+    title: str
     url: str | None
-    username: str
-    password: str = Field(max_length=255)
     notes: str | None = Field(max_length=1000)
+
+
+class CredentialsCreate(CredentialsBase):
+    username: str
+    password: str = Field(min_length=8, max_length=40)
+
+
+class CredentialsUpdate(SQLModel):
+    username: str | None
+    password: str | None = Field(default=None, min_length=8, max_length=40)
+    title: str | None
+    url: str | None
+
+
+class CredentialsPublic(SQLModel):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    title: str
+
+
+class Credentials(CredentialsBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    hashed_password: str
     created_at: datetime = Field(default_factory=timezone.utc)
     updated_at: datetime = Field(
         default_factory=timezone.utc, sa_column_kwargs={"onupdate": timezone.utc}
