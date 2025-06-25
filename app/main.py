1b9e43d123911356a6770677b3f0323802faa8eb
Ughur Huseynli <u.hub.programming@gmail.com>
2025-06-25 14:46:44 +0400

    backend app dockerization done

--- app/main.py
+++ app/main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from app.core.config import settings
 from app.api import main
+from starlette.middleware.cors import CORSMiddleware
 
 
 def custom_generate_unique_id(route: APIRoute) -> str:
+    tag = route.tags[0] if route.tags else "default"
+    return f"{tag}-{route.name}"
     return f"{route.tags[0]}-{route.name}"
 
 
@@ -13,6 +16,15 @@ app = FastAPI(
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
 )
+
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
 
 app.include_router(
     main.api_router,
